f1b2f2448ec47792f241bc3e39090c02b4003f23
n salehi <nimasalehi.auth@icloud.com>
2025-10-20 13:54:44 +0330

    Initial Commit

---
+++ WarehouseArrangementmanual/WarehouseArrangementmanualApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarehouseArrangementmanualApp.swift
+//  WarehouseArrangementmanual
+//
+//  Created by Nima Salehi on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WarehouseArrangementmanualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
